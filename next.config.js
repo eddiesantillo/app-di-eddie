0b7519c456a900669b937d49b9daa7547db5cdce
eddiesantillo <eddiesantillomusic@gmail.com>
2026-08-17 18:54:52 +0200

    Fix next.config.js con serverExternalPackages per Firebase

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  serverExternalPackages: ["firebase", "@firebase/auth"],
 };
 
 module.exports = nextConfig;
